ac875ca80b43877bee1efd6d2587de18b796bdeb
Charlie Van Hook <vanhookch@gmail.com>
2023-10-07 16:20:43 -0400

    distance

--- index.js
+++ index.js
@@ -16,3 +16,95 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+function calculateDistanceToCurrentLocation(location, callback) {
+    const apiKey = 'c4f1309aaa7c4961b4412983dbdbfe18';
+
+    // Function to convert an address to latitude and longitude using the OpenCage Geocoding API
+    async function geocodeAddress(address) {
+        const apiUrl = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}`;
+
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+
+            if (data.results && data.results.length > 0) {
+                const result = data.results[0];
+                const lat = result.geometry.lat;
+                const lng = result.geometry.lng;
+                return { lat, lng };
+            } else {
+                throw new Error("Address not found or an error occurred.");
+            }
+        } catch (error) {
+            throw new Error(`Error geocoding address: ${error.message}`);
+        }
+    }
+
+    // Function to calculate the Haversine distance between two sets of coordinates
+    function calculateHaversineDistance(coord1, coord2) {
+        const earthRadius = 6371; // Radius of the Earth in kilometers
+
+        // Convert latitude and longitude from degrees to radians
+        const lat1 = (Math.PI / 180) * coord1.lat;
+        const lon1 = (Math.PI / 180) * coord1.lng;
+        const lat2 = (Math.PI / 180) * coord2.lat;
+        const lon2 = (Math.PI / 180) * coord2.lng;
+
+        // Haversine formula
+        const dlat = lat2 - lat1;
+        const dlon = lon2 - lon1;
+        const a = Math.sin(dlat / 2) ** 2 + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // Calculate the distance
+        const distance = earthRadius * c;
+        return distance;
+    }
+
+    // Function to get the user's current location
+    async function getUserLocation() {
+        return new Promise((resolve, reject) => {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        resolve({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        });
+                    },
+                    (error) => {
+                        reject(new Error(`Error getting user location: ${error.message}`));
+                    }
+                );
+            } else {
+                reject(new Error("Geolocation is not supported in your browser."));
+            }
+        });
+    }
+
+    (async () => {
+        try {
+            const [locationCoords, userLocation] = await Promise.all([
+                geocodeAddress(location),
+                getUserLocation()
+            ]);
+
+            // Calculate the distance between the given location and the user's location
+            const distance = calculateHaversineDistance(locationCoords, userLocation);
+
+            callback(null, distance);
+        } catch (error) {
+            callback(error);
+        }
+    })();
+}
+
+// Example usage:
+calculateDistanceToCurrentLocation('220 Warren St, Boston, MA 02119', (error, distance) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(`Distance to the given location: ${distance.toFixed(2)} km`);
+    }
+});
